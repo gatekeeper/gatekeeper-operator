88a126b8badc03a9cac0b740d23838ad53bb1d86
Ivan Font <ifont@redhat.com>
2020-09-22 23:13:46 -0700

    Add go-bindata tool dependencies

--- go.mod
+++ go.mod
@@ -3,9 +3,11 @@ module github.com/font/gatekeeper-operator
 go 1.13
 
 require (
+	github.com/go-bindata/go-bindata v3.1.2+incompatible
 	github.com/go-logr/logr v0.1.0
 	github.com/onsi/ginkgo v1.12.1
 	github.com/onsi/gomega v1.10.1
+	github.com/openshift/build-machinery-go v0.0.0-20200917070002-f171684f77ab
 	k8s.io/api v0.18.6
 	k8s.io/apimachinery v0.18.6
 	k8s.io/client-go v0.18.6
---
+++ pkg/tools.go
@@ -0,0 +1,16 @@
+// +build tools
+
+// This package is a placeholder to import tool related code that is not to be
+// included as part of the build. It's a way to allow creating a soft
+// dependency, as otherwise go mod will not import it automatically and a `go
+// mod tidy` will remove it. See
+// https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
+// and
+// https://github.com/go-modules-by-example/index/blob/master/010_tools/README.md
+// for more details.
+package tools
+
+import (
+	_ "github.com/go-bindata/go-bindata/go-bindata"
+	_ "github.com/openshift/build-machinery-go"
+)
