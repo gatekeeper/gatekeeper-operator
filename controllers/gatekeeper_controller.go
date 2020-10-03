4cdbcc9b1ffc23b4cf4e4d568b36d1d4fd71f254
Ivan Font <ifont@redhat.com>
2020-10-02 18:01:12 -0700

    Add ability to deploy Gatekeeper resources

--- controllers/gatekeeper_controller.go
+++ controllers/gatekeeper_controller.go
@@ -18,13 +18,39 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/go-logr/logr"
+	"github.com/openshift/library-go/pkg/manifest"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	operatorv1alpha1 "github.com/font/gatekeeper-operator/api/v1alpha1"
+	"github.com/font/gatekeeper-operator/pkg/bindata"
+)
+
+var (
+	defaultGatekeeperCrName = "gatekeeper"
+	staticAssetsDir         = "config/gatekeeper/"
+	orderedStaticAssets     = []string{
+		"apiextensions.k8s.io_v1beta1_customresourcedefinition_configs.config.gatekeeper.sh.yaml",
+		"apiextensions.k8s.io_v1beta1_customresourcedefinition_constrainttemplates.templates.gatekeeper.sh.yaml",
+		"apiextensions.k8s.io_v1beta1_customresourcedefinition_constrainttemplatepodstatuses.status.gatekeeper.sh.yaml",
+		"apiextensions.k8s.io_v1beta1_customresourcedefinition_constraintpodstatuses.status.gatekeeper.sh.yaml",
+		"~g_v1_namespace_gatekeeper-system.yaml",
+		"~g_v1_secret_gatekeeper-webhook-server-cert.yaml",
+		"~g_v1_serviceaccount_gatekeeper-admin.yaml",
+		"rbac.authorization.k8s.io_v1_clusterrole_gatekeeper-manager-role.yaml",
+		"rbac.authorization.k8s.io_v1_clusterrolebinding_gatekeeper-manager-rolebinding.yaml",
+		"rbac.authorization.k8s.io_v1_role_gatekeeper-manager-role.yaml",
+		"rbac.authorization.k8s.io_v1_rolebinding_gatekeeper-manager-rolebinding.yaml",
+		"apps_v1_deployment_gatekeeper-audit.yaml",
+		"apps_v1_deployment_gatekeeper-controller-manager.yaml",
+		"~g_v1_service_gatekeeper-webhook-service.yaml",
+		"admissionregistration.k8s.io_v1beta1_validatingwebhookconfiguration_gatekeeper-validating-webhook-configuration.yaml",
+	}
 )
 
 // GatekeeperReconciler reconciles a Gatekeeper object
@@ -38,10 +64,29 @@ type GatekeeperReconciler struct {
 // +kubebuilder:rbac:groups=operator.gatekeeper.sh,namespace="system",resources=gatekeepers/status,verbs=get;update;patch
 
 func (r *GatekeeperReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
-	_ = context.Background()
-	_ = r.Log.WithValues("gatekeeper", req.NamespacedName)
+	ctx := context.Background()
+	logger := r.Log.WithValues("gatekeeper", req.NamespacedName)
+	logger.Info("Reconciling Gatekeeper")
 
-	// your logic here
+	if req.Name != defaultGatekeeperCrName {
+		err := fmt.Errorf("Gatekeeper resource name must be '%s'", defaultGatekeeperCrName)
+		logger.Error(err, "Invalid Gatekeeper resource name")
+		// Return success to avoid requeue
+		return ctrl.Result{}, nil
+	}
+
+	gatekeeper := &operatorv1alpha1.Gatekeeper{}
+	err := r.Get(ctx, req.NamespacedName, gatekeeper)
+	if err != nil {
+		// Reconcile failed due to error - requeue
+		return ctrl.Result{}, err
+	}
+
+	err = r.deployGatekeeperResources()
+	if err != nil {
+		err := fmt.Errorf("Unable to deploy Gatekeeper resources: %v", err)
+		return ctrl.Result{}, err
+	}
 
 	return ctrl.Result{}, nil
 }
@@ -50,4 +95,34 @@ func (r *GatekeeperReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&operatorv1alpha1.Gatekeeper{}).
 		Complete(r)
+}
+
+func (r *GatekeeperReconciler) deployGatekeeperResources() error {
+	ctx := context.Background()
+	var err error
+	for _, a := range orderedStaticAssets {
+		assetName := staticAssetsDir + a
+		bytes, err := bindata.Asset(assetName)
+		if err != nil {
+			return err
+		}
+
+		logger := r.Log.WithValues("Gatekeeper resource", string(assetName))
+		manifest := &manifest.Manifest{}
+		err = manifest.UnmarshalJSON(bytes)
+		if err != nil {
+			return err
+		}
+
+		err = r.Create(ctx, manifest.Obj)
+		if err != nil {
+			if !errors.IsAlreadyExists(err) {
+				return err
+			}
+		} else {
+			logger.Info(fmt.Sprintf("Created Gatekeeper resource %s", assetName))
+		}
+	}
+
+	return err
 }
