aa2006f65df9496c618b3493734c45ed7ad4e3ce
Ivan Font <ifont@redhat.com>
2021-06-23 17:13:41 -0700

    Fix comment

--- pkg/tools.go
+++ pkg/tools.go
@@ -18,7 +18,7 @@ limitations under the License.
 // included as part of the operator binary because the build target is
 // something non-existent. It's a way to allow creating a soft dependency, as
 // otherwise go mod will not import it automatically and a `go mod tidy` will
-// remove it.  Since the build target is something nonSee
+// remove it. See
 // https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
 // and
 // https://github.com/go-modules-by-example/index/blob/master/010_tools/README.md
